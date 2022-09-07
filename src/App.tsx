4a6d80f39aa45ca4b2b92bf3956d00a351ae9f87
Maarten de Rijk <maarten.rijk@rhdhv.com>
2022-09-07 21:54:16 +0200

    independent of hosting

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,9 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
 import ButtonAppBar from './components/AppBar';
 import AudioPlayer from './components/AudioPlayer';
+import { songData } from './static/tasks';
 
 function App() {
-  const [songData, setSongData] = React.useState<jsonData[]>([]);
-
-  type jsonData = {
-    id: number;
-    name: string;
-    source: string;
-  }
-
-  React.useEffect(() => {
-    const getData = async () => {
-      const result = await fetch('https://s3.eu-west-1.amazonaws.com/rhdhv.maarten/Heardle/media/songlist.json');
-      const json = await result.json() as jsonData[];
-      setSongData(json)
-    }
-    getData()
-  }, []);
 
   return (
     <React.Fragment>
---
+++ src/static/tasks.ts
@@ -0,0 +1,99 @@
+const baseSite = process.env.PUBLIC_URL;
+
+export const songData = [
+    {
+        "source": `${baseSite}/media/19.mp3`,
+        "name": "#Heardle #195",
+        "id": 167
+    },
+    {
+        "source": `${baseSite}/media/18.mp3`,
+        "name": "#Heardle #194",
+        "id": 161
+    },
+    {
+        "source": `${baseSite}/media/17.mp3`,
+        "name": "#Heardle #193",
+        "id": 57
+    },
+    {
+        "source": `${baseSite}/media/16.mp3`,
+        "name": "#Heardle #192",
+        "id": 160
+    },
+    {
+        "source": `${baseSite}/media/15.mp3`,
+        "name": "#Heardle #191",
+        "id": 151
+    },
+    {
+        "source": `${baseSite}/media/14.mp3`,
+        "name": "#Heardle #190",
+        "id": 143
+    },
+    {
+        "source": `${baseSite}/media/13.mp3`,
+        "name": "#Heardle #189",
+        "id": 23
+    },
+    {
+        "source": `${baseSite}/media/12.mp3`,
+        "name": "#Heardle #188",
+        "id": 142
+    },
+    {
+        "source": `${baseSite}/media/11.mp3`,
+        "name": "#Heardle #187",
+        "id": 138
+    },
+    {
+        "source": `${baseSite}/media/10.mp3`,
+        "name": "#Heardle #186",
+        "id": 130
+    },
+    {
+        "source": `${baseSite}/media/9.mp3`,
+        "name": "#Heardle #185",
+        "id": 125
+    },
+    {
+        "source": `${baseSite}/media/8.mp3`,
+        "name": "#Heardle #184",
+        "id": 122
+    },
+    {
+        "source": `${baseSite}/media/7.mp3`,
+        "name": "#Heardle #183",
+        "id": 119
+    },
+    {
+        "source": `${baseSite}/media/6.mp3`,
+        "name": "#Heardle #182",
+        "id": 112
+    },
+    {
+        "source": `${baseSite}/media/5.mp3`,
+        "name": "#Heardle #181",
+        "id": 109
+    },
+    {
+        "source": `${baseSite}/media/4.mp3`,
+        "name": "#Heardle #180",
+        "id": 104
+    },
+    {
+        "source": `${baseSite}/media/3.mp3`,
+        "name": "#Heardle #179",
+        "id": 98
+    },
+    {
+        "source": `${baseSite}/media/2.mp3`,
+        "name": "#Heardle #178",
+        "id": 92
+    },
+    {
+        "source": `${baseSite}/media/1.mp3`,
+        "name": "#Heardle #177",
+        "id": 111
+    }
+]
